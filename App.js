df01cb9017957e0e1d36b8a19b250152a14c9a52
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-10-29 15:14:33 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today´s Tasks</Text>
+        <View style={styles.items}>
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+        </View>  
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e8eaed',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+      marginTop: 30,
+  },
+});
